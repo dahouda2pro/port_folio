70e8837af31b4ad81cf096364c1b4a2c16d408c8
dahouda2pro <dahouda37@gmail.com>
2023-04-13 14:50:02 +0900

    Update Name

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -10,7 +10,7 @@ const Hero = (props) => (
       <LeftSection>
         <SectionTitle main center>
           Welcome To <br />
-          My Personal Portfolio
+          Dahouda MWAMBA
         </SectionTitle>
         <SectionText>
         The purpose of JavaScript Mastery is to help aspiring and established developers to take their development skills to the next level and build awesome apps.
