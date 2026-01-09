19f6767195b81b16fa82b64b794c5d8a2d2fd924
Dahouda MWAMBA <46145944+dahouda2pro@users.noreply.github.com>
2026-01-09 10:41:28 +0000

    Updating Profile

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -10,14 +10,13 @@ const Hero = (props) => (
       <LeftSection>
         <SectionTitle main center>
           Dahouda <br />
-          MWAMBA KASONGO
+          MWAMBA KASONGO, Ph.D.
         </SectionTitle>
-        💼 Software Engineer | Machine & Deep Learning Engineer | Big Data Developer <br />
+        <SectionText>
+       Software Engineer | Machine & Deep Learning Engineer | Big Data Developer <br />
        I am a versatile engineer passionate about building scalable software, designing and deploying AI/ML models, and processing large-scale data. <br />
        With expertise in Python, SQL, big data frameworks, and machine learning pipelines [Kedro, ZenML and Apache Airflow], <br />
        I solve real-world problems by bridging software development, AI, and data engineering.
-        <SectionText>
-       software engineer
         </SectionText>
         <Button onClick={props.handleClick}>Learn More</Button>
       </LeftSection>
