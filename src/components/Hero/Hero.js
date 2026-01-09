b55e77e356e0855af1411d62d2ef86eae045b399
Dahouda MWAMBA <46145944+dahouda2pro@users.noreply.github.com>
2026-01-09 09:30:52 +0000

    Changing Profile Information

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -13,8 +13,8 @@ const Hero = (props) => (
           Dahouda MWAMBA
         </SectionTitle>
         <SectionText>
-        Mwamba Kasongo Dahouda received a BS in information system engineering from the University Protestant of Lubumbashi, Lubumbashi, DR Congo, and the MS in software engineering from Hanyang University, Seoul, Korea in 2020.
-        <br /> He is currently pursuing a Ph.D. in software engineering at Hanyang University, Seoul, Korea
+       Mwamba Kasongo Dahouda received a BS in information system engineering from the University Protestant of Lubumbashi, Lubumbashi, DR Congo, 
+       <br /> an MS in software engineering from Hanyang University, Seoul, Korea in 2020; and a Ph.D. in software engineering at Hanyang University, Seoul, Korea
         </SectionText>
         <Button onClick={props.handleClick}>Learn More</Button>
       </LeftSection>
